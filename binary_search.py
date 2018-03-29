d9a74ce2602a5ff974bf117bcd624da0cb88ddb2
qazqaz12378 <290513701@qq.com>
2018-03-29 14:12:23 +0800

    hyc
    二分法查找

---
+++ binary_search.py
@@ -0,0 +1,15 @@
+import math
+
+def binary_search(list,item):
+	low = 0
+	high = len(list) - 1
+	while low <= high:
+		mid = math.ceil((low + high) / 2)
+		guess = list[mid]		
+		if guess == item:
+			return mid
+		if guess > item:
+			high = mid - 1
+		else:
+			low = mid + 1		
+	return None
